d883c7972c2e062b5529f4914a541f2b1bc20a35
Hieu Nguyen <davidthehieu@gmail.com>
2025-02-26 12:06:09 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+        <img src="/roof.png" alt="Food Truck Roof" />
+        <h1>Food Truck Favorites</h1>
+        
+      
+    </div>
+  )
+}
+
+export default App
