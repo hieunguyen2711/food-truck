0cde3c2e764f6d1e0319b6d05d427a12be131ba7
Hieu Nguyen <davidthehieu@gmail.com>
2025-02-28 11:43:03 -0600

    Added the images to public directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,5 @@
 import './App.css';
 import FoodTruck from './components/FoodTruck';
-import tacosPic from "./components/images/tacos-pic.webp";
-import indianPic from "./components/images/indian-pic.jpg";
-import halalPic from "./components/images/halal-pic.webp";
-import nyDosas from "./components/images/nydosas-pic.jpg";
-import jamaicanPic from "./components/images/jamaican-pic.jpg";
-import bangaliPic from "./components/images/bangali-pic.jpg";
-import greekPic from "./components/images/greek-pic.jpg";
-import chinaPic from "./components/images/china-pic.jpg";
-import gussyPic from "./components/images/gussy-pic.jpg";
-import pantaconPic from "./components/images/patacon-pic.jpg";
-import momoPic from "./components/images/momo-pic.jpg";
-import makinaPic from "./components/images/makina-pic.jpg";
-
-
 
 const App = () => {
 
@@ -24,40 +10,40 @@ const App = () => {
           <h1>Food Truck Favorites</h1>
         <div class="card-list" > 
             <div className="Card">
-                <FoodTruck src={tacosPic} truckName='Test' country='Vietnam' alt='Test-pic'/>
+                <FoodTruck src="/tacos-pic.webp" truckName='Test' country='Vietnam' alt='Test-pic'/>
             </div>
             <div className="Card">
-                <FoodTruck src={indianPic} truckName='' country=''/>
+                <FoodTruck src="/indian-pic.jpg" truckName='' country=''/>
             </div>
             <div className="Card">
-                <FoodTruck src={halalPic} truckName='' country=''/>
+                <FoodTruck src="/halal-pic.webp" truckName='' country=''/>
             </div>
             <div className="Card">
-                <FoodTruck src={nyDosas} truckName='' country=''/>
+                <FoodTruck src="/nydosas-pic.jpg" truckName='' country=''/>
             </div>
             <div className="Card">
-                <FoodTruck src={jamaicanPic} truckName='' country=''/>
+                <FoodTruck src="/jamaican-pic.jpg" truckName='' country=''/>
             </div>
             <div className="Card">
-                <FoodTruck src={bangaliPic} truckName='' country=''/>
+                <FoodTruck src="/bangali-pic.jpg" truckName='' country=''/>
             </div>
             <div className="Card">
-                <FoodTruck src={greekPic} truckName='' country=''/>
+                <FoodTruck src="/greek-pic.jpg" truckName='' country=''/>
             </div>
             <div className="Card">
-                <FoodTruck src={chinaPic} truckName='' country=''/>
+                <FoodTruck src="/china-pic.jpg" truckName='' country=''/>
             </div>
             <div className="Card">
-                <FoodTruck src={gussyPic} truckName='' country=''/>
+                <FoodTruck src="/gussy-pic.jpg" truckName='' country=''/>
             </div>
             <div className="Card">
-                <FoodTruck src={pantaconPic} truckName='' country=''/>
+                <FoodTruck src="/patacon-pic.jpg" truckName='' country=''/>
             </div>
             <div className="Card">
-                <FoodTruck src={momoPic} truckName='' country=''/>
+                <FoodTruck src="/momo-pic.jpg" truckName='' country=''/>
             </div>
             <div className="Card">
-                <FoodTruck src={makinaPic} truckName='' country=''/>
+                <FoodTruck src="/makina-pic.jpg" truckName='' country=''/>
             </div>
         
 
